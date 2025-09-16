21f908c417e777d91e3a15f8a72858ec93292b6d
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-16 12:08:30 +0000

    task 3 & 4

--- Typescript/task_1/js/main.ts
+++ Typescript/task_1/js/main.ts
@@ -68,9 +68,11 @@ class StudentClass implements StudentClassInterface {
     age: number
     location: string
 
-    constructor(firstName: string, lastName: string) {
+    constructor(firstName: string, lastName: string, age: number, location: string) {
         this.firstName = firstName
         this.lastName = lastName
+         this.age = age
+        this.location = location
     }
 
     workOnHomework(): string {
