28f6b6d71b8b6722bf370bbab70aacbc685d46f7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-18 13:23:21 +0000

    task 3

---
+++ Typescript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import { RowID, RowElement } from "./interface";
+
+declare function insertRow(row: RowElement):RowID
+declare function deleteRow(rowId: RowID): void
+declare function updateRow(rowId: RowID, row: RowElement): RowID
